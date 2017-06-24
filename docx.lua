3f25690a4ccf26be4ad7c4a1f3dfe75b6e3a49ed
paragasu <paragasu@gmail.com>
2017-06-24 16:44:55 +0800

    ngx.NOTICE to ngx.ERR

--- docx.lua
+++ docx.lua
@@ -113,9 +113,9 @@ function m:clean_docx_xml(input_docx)
   if not input_docx then error("Missing input file " .. i(input_docx)) end
   local prog = exec.new(sock_file)
   local cmd  = string.format('libreoffice --headless --convert-to docx:"MS Word 2007 XML" --outdir %s %q', self.tmp_dir, input_docx)
-  ngx.log(ngx.NOTICE, "docx: " .. cmd)
+  ngx.log(ngx.ERR, "docx: " .. cmd)
   local res, err = prog('bash', '-c', cmd);
-  ngx.log(ngx.NOTICE, "cmd result", i(res), i(err)) 
+  ngx.log(ngx.ERR, "cmd result", i(res), i(err)) 
   if res and string.find(res.stdout, "using filter") then 
     --m.set_file_writeable(self.tmp_dir .. '/' .. m.get_filename(docx_file))
     return true 
