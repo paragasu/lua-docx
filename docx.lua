a8d77908081a63d9a934a18a4df1f50eb03bd963
paragasu <paragasu@gmail.com>
2018-01-03 03:04:19 +0800

    refactorg

--- docx.lua
+++ docx.lua
@@ -18,10 +18,10 @@ function m.new(tmp_dir)
   ngx.log(ngx.NOTICE, "tmp_dir: " .. self.tmp_dir)
 
   function self:replace_tags(filepath, tags)
-    m:validate_file(filepath)
+    self:validate_file(filepath)
     self.docx = filepath;
     self.ar = assert(zip.open(self.docx)) 
-    self.escaped_tags = m:escape_xml_chars(tags)
+    self.escaped_tags = self:escape_xml_chars(tags)
 
     ngx.log(ngx.NOTICE, "open zip " .. self.docx)
     -- file must be writeable
@@ -51,7 +51,7 @@ function m.new(tmp_dir)
   end
 
   -- get the content of xml file inside the zip
-  function m:replace_docx_xml_content(idx)
+  function self:replace_docx_xml_content(idx)
     local file = assert(self.ar:open(idx))
     local stat = self.ar:stat(idx) 
     local tpl  = file:read(stat.size) 
@@ -59,29 +59,27 @@ function m.new(tmp_dir)
     file:close()
   end
 
-  function m:replace_xml_tags(tpl)
+  function self:replace_xml_tags(tpl)
     return string.gsub(tpl, '#%a+%.[%a%s%d]+#', self.escaped_tags) or ''
+  end
+
+  function self:validate_file(filepath)
+    if type(filepath) ~= 'string' then error('Invalid docx file') end
+    if not string.match(filepath, '%.docx') then error('Only docx file supported ' .. filepath) end
+    if string.find(filepath, '%.%/') then error('Relative path using ./ not supported ' .. filepath) end
+    if string.find(filepath, '%~%/') then error('Relative path using ~/ not supported ' .. filepath) end
+    if not u.file_exists(filepath) then error('File '.. filepath .. ' not exists') end
+  end
+
+  function self:escape_xml_chars(tags)
+    local data = {}
+    for tag, value in pairs(tags) do
+      data[tag] = u.xml_escape_chars(value)
+    end
+    return data or ''
   end
 
   return self
 end
-
-
-function m:validate_file(filepath)
-  if type(filepath) ~= 'string' then error('Invalid docx file') end
-  if not string.match(filepath, '%.docx') then error('Only docx file supported ' .. filepath) end
-  if string.find(filepath, '%.%/') then error('Relative path using ./ not supported ' .. filepath) end
-  if string.find(filepath, '%~%/') then error('Relative path using ~/ not supported ' .. filepath) end
-  if not u.file_exists(filepath) then error('File '.. filepath .. ' not exists') end
-end
-
-function m:escape_xml_chars(tags)
-  local data = {}
-  for tag, value in pairs(tags) do
-    data[tag] = u.xml_escape_chars(value)
-  end
-  return data or ''
-end
-
 
 return m
